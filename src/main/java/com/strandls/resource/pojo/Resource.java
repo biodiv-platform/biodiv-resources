344a7d62fc1a595663864d427d678931c1415d32
prakhar-s <prakhardevsingh4@gmail.com>
2025-10-06 08:55:52 +0530

    refactor: :recyle: testing a different sequence generation strategy

--- src/main/java/com/strandls/resource/pojo/Resource.java
+++ src/main/java/com/strandls/resource/pojo/Resource.java
@@ -82,7 +82,7 @@ public class Resource implements Serializable {
 	}
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	public Long getId() {
 		return id;
